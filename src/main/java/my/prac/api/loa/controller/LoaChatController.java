af7a44345e7486f986e4219629c9100db1b7e359
THMAIN <THMAIN@DESKTOP-FABFR4R>
2024-01-30 22:32:03 +0900

    모험섬테스트

--- src/main/java/my/prac/api/loa/controller/LoaChatController.java
+++ src/main/java/my/prac/api/loa/controller/LoaChatController.java
@@ -10,7 +10,10 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +24,6 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-import javax.servlet.http.HttpServletRequest;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -36,7 +38,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 @Controller
 public class LoaChatController {
 	static Logger logger = LoggerFactory.getLogger(LoaChatController.class);
@@ -48,23 +49,21 @@ public class LoaChatController {
 	final String lostArkAPIurl = "https://developer-lostark.game.onstove.com";
 
 	@RequestMapping(value = "/loa/chat", method = RequestMethod.GET)
-	public @ResponseBody Map<String,Object> chatApplication(
-			@RequestParam(required = true) String action,
+	public @ResponseBody Map<String, Object> chatApplication(@RequestParam(required = true) String action,
 			@RequestParam(required = true) String param) {
 		HashMap<String, Object> rtnMap = new HashMap<>();
-		
+
 		try {
-			System.out.println(action+" "+param);
-			switch(action) {
-				case "equipment":
-					return equipmentSearch(param);
-				case "calendar":
-					return calendarSearch(param);
-				case "weather":
-					return weatherSearch(param);
+			System.out.println(action + " " + param);
+			switch (action) {
+			case "equipment":
+				return equipmentSearch(param);
+			case "calendar":
+				return calendarSearch(param);
+			case "weather":
+				return weatherSearch(param);
 			}
 			rtnMap.put("CODE", "OK");
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -73,42 +72,66 @@ public class LoaChatController {
 
 		return rtnMap;
 	}
-	
-	Map<String,Object> calendarSearch(String userId) throws Exception{
-HashMap<String, Object> rtnMap = new HashMap<>();
-		
+
+	Map<String, Object> calendarSearch(String userId) throws Exception {
+		HashMap<String, Object> rtnMap = new HashMap<>();
+		String retMsg="오늘의 모험섬";
 		userId = URLEncoder.encode(userId, "UTF-8");
-		//+는 %2B로 치환한다 
-		String param = lostArkAPIurl + "/gamecontents/calendar";
-		URL url = new URL(param);
-
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		// POST 요청을 위해 기본값이 false인 setDoOutput을 true로
-
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Accept", "application/json");
-		conn.setRequestProperty("Authorization", lostArkKey);
-
-		// 결과 코드가 200이라면 성공
-		int responseCode = conn.getResponseCode();
-		System.out.println("### getAccessToken responseCode : " + responseCode);
-
-		// 서버로부터 데이터 읽어오기
-		StringBuilder sb = new StringBuilder();
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				sb.append(line).append("\n");
-			}
-		}
+		// +는 %2B로 치환한다
+		String paramUrl = lostArkAPIurl + "/gamecontents/calendar";
 		
-		System.out.println(sb.toString());
-		List<Map<String, Object>> mapList = new ObjectMapper().readValue(sb.toString(), new TypeReference<List<Map<String, Object>>>() {
+		String returnData = connect_process(paramUrl);
+		List<Map<String, Object>> data_list = new ObjectMapper().readValue(returnData, new TypeReference<List<Map<String, Object>>>() {
 		});
-		 
 		
+		for(Map<String,Object> data_sub_list : data_list) {
+			
+			if(data_sub_list.get("CategoryName").equals("모험 섬")) {
+				
+				
+				
+				List<Map<String, Object>> rewardItemsList = (List<Map<String, Object>>)data_sub_list.get("RewardItems");
+				for(Map<String, Object> rewardItem : rewardItemsList) {
+					
+					if(rewardItem.get("StartTimes")!=null) {
+						List<String> start_time_list = (List<String>)rewardItem.get("StartTimes");
+						for(String time : start_time_list) {
+							if(time.equals(StringToDate()+"T11:00:00")) {
+								retMsg = retMsg + "</br>";
+								
+								switch(rewardItem.get("Name").toString()) {
+									case "전설 ~ 고급 카드 팩 III":
+										retMsg = retMsg + data_sub_list.get("Location").toString()+" : ";
+										retMsg+="카드";
+										break;
+									case "실링":
+										retMsg = retMsg + data_sub_list.get("Location").toString()+" : ";
+										retMsg+="실링";
+										break;
+									case "대양의 주화 상자":
+										retMsg = retMsg + data_sub_list.get("Location").toString()+" : ";
+										retMsg+="주화";
+										break;
+									case "골드":
+										retMsg = retMsg + data_sub_list.get("Location").toString()+" : ";
+										retMsg+="골드";
+										break;
+									default:
+										continue;
+								}
+							}
+						}
+					}
+					
+				}
+				
+			}
+			
+		}
+		System.out.println(retMsg);
+		rtnMap.put("data", retMsg);
 		
-		//System.out.println(rtnMap);
+		// System.out.println(rtnMap);
 		/*
 		 * List<Map<String,Object>> armoryEquipment = (List<Map<String,Object>>)
 		 * rtnMap.get("ArmoryEquipment");
@@ -119,20 +142,41 @@ HashMap<String, Object> rtnMap = new HashMap<>();
 		 * 
 		 * }
 		 */
-		
-		rtnMap.put("data", mapList);
+
 		return rtnMap;
 	}
-	
-	Map<String,Object> equipmentSearch(String userId) throws Exception{
+
+	Map<String, Object> equipmentSearch(String userId) throws Exception {
 		HashMap<String, Object> rtnMap = new HashMap<>();
-		
+		String retMsg="";
 		userId = URLEncoder.encode(userId, "UTF-8");
-		//+는 %2B로 치환한다 
-		String param = lostArkAPIurl + "/armories/characters/" + userId + "?filters=equipment";
+		// +는 %2B로 치환한다
+		String paramUrl = lostArkAPIurl + "/armories/characters/" + userId + "?filters=equipment";
+
+		String returnData = connect_process(paramUrl);
 		
-		// URL url = new URL(lostArkAPIurl+"/characters/"+userId+"/siblings");
-		URL url = new URL(param);
+		Map<String, Object> map = new ObjectMapper().readValue(returnData, new TypeReference<Map<String, Object>>() {
+		});
+		
+		// System.out.println(rtnMap);
+
+		//List<Map<String, Object>> armoryEquipment = (List<Map<String, Object>>) map_list.get("ArmoryEquipment");
+		/*
+		 * String equipOne = ""; for (Map<String, Object> equip : armoryEquipment) {
+		 * System.out.println("==================="); System.out.println(equip);
+		 * equipOne = equip.toString();
+		 * 
+		 * }
+		 */
+		rtnMap.put("data", map);
+		return rtnMap;
+	}
+
+	
+	String connect_process(String paramUrl) throws Exception {
+		List<Map<String, Object>>rtnMap = new ArrayList<>();
+		
+		URL url = new URL(paramUrl);
 
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		// POST 요청을 위해 기본값이 false인 setDoOutput을 true로
@@ -153,61 +197,49 @@ HashMap<String, Object> rtnMap = new HashMap<>();
 				sb.append(line).append("\n");
 			}
 		}
-		
-		rtnMap = new ObjectMapper().readValue(sb.toString(), new TypeReference<Map<String, Object>>() {
-		});
-		
-		//System.out.println(rtnMap);
-		
-		List<Map<String,Object>> armoryEquipment = (List<Map<String,Object>>) rtnMap.get("ArmoryEquipment");
-		
-		String equipOne = "";
-		for(Map<String,Object> equip: armoryEquipment){
-			System.out.println("===================");
-			System.out.println(equip);
-			equipOne=equip.toString();
-			
-		}
-		
-		rtnMap.put("data", equipOne);
-		return rtnMap;
+
+		return sb.toString();
 	}
 	
-	Map<String,Object> weatherSearch(String area) throws Exception{
+	
+	
+	Map<String, Object> weatherSearch(String area) throws Exception {
 		HashMap<String, Object> rtnMap = new HashMap<>();
-		
+
 		String retMsg = "";
 		try {
 			setSSL();
 			String WeatherURL = "https://m.search.naver.com/search.naver?&query=날씨+" + area;
 			Document doc = Jsoup.connect(WeatherURL).get();
 			String cur_temp = doc.select(".weather_info ._today .temperature_text strong").text();
-			String weather =  doc.select(".weather_info ._today .before_slash").text();
+			String weather = doc.select(".weather_info ._today .before_slash").text();
 			String diff_temp = doc.select(".weather_info ._today .temperature_info .temperature").text();// 어제와 온도차이
 			String v1 = doc.select(".weather_info ._today .summary_list .sort:eq(0) .desc").text();// 체감
 			String v2 = doc.select(".weather_info ._today .summary_list .sort:eq(1) .desc").text();// 습도
 			String v3 = doc.select(".weather_info ._today .summary_list .sort:eq(2) .desc").text();// 풍속
 			retMsg += "오늘날씨 : " + weather;
-			retMsg += " </br>현재온도 : " + cur_temp;
-			retMsg += " </br>체감온도 : " + v1;
-			retMsg += " </br>습도 : " + v2;
-			retMsg += " </br>풍속 : " + v3;
+			retMsg += "</br>현재온도 : " + cur_temp;
+			retMsg += "</br>체감온도 : " + v1;
+			retMsg += "</br>습도 : " + v2;
+			retMsg += "</br>풍속 : " + v3;
 			retMsg += "</br>현재 " + area + "의 온도는 " + cur_temp + " 이며 어제보다 " + diff_temp;
 		} catch (Exception e) {
 			e.printStackTrace();
-			retMsg ="불러올 수 없는 지역이거나 지원되지 않는 지역입니다.";
+			retMsg = "불러올 수 없는 지역이거나 지원되지 않는 지역입니다.";
 		}
 		rtnMap.put("data", retMsg);
 		return rtnMap;
 	}
-
+	
 	public void setSSL() throws NoSuchAlgorithmException, KeyManagementException {
 		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
 			public X509Certificate[] getAcceptedIssuers() {
 				return null;
 			}
+
 			public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 			}
+
 			public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 			}
 		} };
@@ -219,5 +251,12 @@ HashMap<String, Object> rtnMap = new HashMap<>();
 			}
 		});
 		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	}
+
+	public String StringToDate() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateTime = dateFormat.format(new Date());
+        
+        return dateTime;
 	}
 }
