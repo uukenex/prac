aa35f5dc131d13b3499150298a0bcdb9dfff2c41
THMAIN <THMAIN@DESKTOP-FABFR4R>
2024-02-13 21:32:09 +0900

    줘 이미지

--- src/main/java/my/prac/api/loa/controller/LoaChatController.java
+++ src/main/java/my/prac/api/loa/controller/LoaChatController.java
@@ -100,8 +100,10 @@ public class LoaChatController {
 				val = list;
 				break;
 			case "/줘":
+				
+				Random random2 = new Random();
 				param0=param0.substring(1);
-				val = "imgwww.dev-apc.com"+URLEncoder.encode(param0, "UTF-8");
+				val = "imgwww.dev-apc.com/"+URLEncoder.encode(param0, "UTF-8")+"v="+random2.nextInt(100);
 				break;
 			case "/모험섬":
 				val = calendarSearch();
