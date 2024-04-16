3a7d703fb92761bc9063c33ad041603c0b9454a0
uukenex <uukenex@kakao.com>
2024-04-16 17:04:49 +0900

    이미지 조정

--- src/main/webapp/WEB-INF/game_set/js/comutil.js
+++ src/main/webapp/WEB-INF/game_set/js/comutil.js
@@ -31,25 +31,24 @@ function resize_event(arg){
 		$('img').css('height',Math.round( se2_board_width * 0.9) );
 		*/
 		$('img').css('width','100%');
-		$('img').css('height','100%');
+		$('img').css('height','auto');
 		$('img').css('object-fit','contain');
 		//console.log(se2_board_width);
 	}else{
+		
+		$('img').css('height','auto');
+		
 		if(resizeWindowWidth > 1800){
 			$('img').css('width',Math.round( resizeWindowWidth * 0.3) );
-			$('img').css('height',Math.round( resizeWindowWidth * 0.3) );
 			$('img').css('object-fit','contain');
 		}else if(resizeWindowWidth > 1500){
 			$('img').css('width',Math.round( resizeWindowWidth * 0.4) );
-			$('img').css('height',Math.round( resizeWindowWidth * 0.4) );
 			$('img').css('object-fit','contain');
 		}else if(resizeWindowWidth > 800){
 			$('img').css('width',Math.round( resizeWindowWidth * 0.5) );
-			$('img').css('height',Math.round( resizeWindowWidth * 0.5) );
 			$('img').css('object-fit','contain');
 		}else{
 			$('img').css('width',Math.round( resizeWindowWidth * 0.7) );
-			$('img').css('height',Math.round( resizeWindowWidth * 0.7) );
 			$('img').css('object-fit','contain');
 		}
 		
