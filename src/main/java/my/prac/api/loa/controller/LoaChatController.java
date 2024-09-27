d37e3b1e15ccbb392468c4abbe9d329d1c28b89d
thjeon <thjeon@172.16.217.72>
2024-09-27 16:25:33 +0900

    고대3 검색추가

--- src/main/java/my/prac/api/loa/controller/LoaChatController.java
+++ src/main/java/my/prac/api/loa/controller/LoaChatController.java
@@ -1499,6 +1499,7 @@ public class LoaChatController {
 			
 			switch (equip.get("Type").toString()) {
 			case "어빌리티 스톤":
+				/*
 				HashMap<String, Object> stone_val = (HashMap<String, Object>) stone_element.get("value");
 				if(stone_val == null || stone_val.size() ==0 ) {
 					continue;
@@ -1525,6 +1526,7 @@ public class LoaChatController {
 				
 				resMsg += stone_option1_str + " " + stone_option2_str +enterStr;
 				break;
+				*/
 			case "반지":case "귀걸이": case "목걸이":
 				switch(Jsoup.parse((String) ((HashMap<String, Object>) quality_element.get("value")).get("leftStr2")).text()) {
 					case "아이템 티어 3":
@@ -1947,6 +1949,8 @@ public class LoaChatController {
 		resMsg += enterStr;
 		resMsg += "[고대 3연마]"+enterStr;
 		resMsg +=searchAuctionParse("고대3낙인력");
+		resMsg +=searchAuctionParse("고대3공퍼");
+		resMsg +=searchAuctionParse("고대3치적");
 		resMsg += enterStr;
 		resMsg += "[고대 1연마]"+enterStr;
 		resMsg +=searchAuctionParse("고대1낙인력");
@@ -2065,6 +2069,54 @@ public class LoaChatController {
 			json.put("EtcOptions",options);
 			
 			resMsg +="목걸이(낙인력 상) ";
+			resMsg += auctionSearchDt(json,false,true);
+			break;
+		case "고대3공퍼":
+			//resMsg += "[고대 1연마]낙인력 목걸이"+enterStr;
+			json.put("CategoryCode", "200020");//20귀걸이
+			json.put("Sort", "BUY_PRICE");
+			json.put("SortCondition", "ASC");
+			json.put("ItemGrade", "고대");
+			
+			json2.put("FirstOption",8);
+			json2.put("SecondOption",1);
+			json2.put("MinValue",12);
+			json2.put("MaxValue",12);
+			options.put(json2);
+			
+			json3.put("FirstOption",7);
+			json3.put("SecondOption",45);//45 공퍼
+			json3.put("MinValue",3);
+			json3.put("MaxValue",3);
+			options.put(json3);
+			
+			json.put("EtcOptions",options);
+			
+			resMsg +="목걸이(공% 상) ";
+			resMsg += auctionSearchDt(json,false,true);
+			break;
+		case "고대3치적":
+			//resMsg += "[고대 1연마]낙인력 목걸이"+enterStr;
+			json.put("CategoryCode", "200030");//30반지
+			json.put("Sort", "BUY_PRICE");
+			json.put("SortCondition", "ASC");
+			json.put("ItemGrade", "고대");
+			
+			json2.put("FirstOption",8);
+			json2.put("SecondOption",1);
+			json2.put("MinValue",12);
+			json2.put("MaxValue",12);
+			options.put(json2);
+			
+			json3.put("FirstOption",7);
+			json3.put("SecondOption",49);
+			json3.put("MinValue",3);
+			json3.put("MaxValue",3);
+			options.put(json3);
+			
+			json.put("EtcOptions",options);
+			
+			resMsg +="반지(치적% 상) ";
 			resMsg += auctionSearchDt(json,false,true);
 			break;
 		case "고대1낙인력":
