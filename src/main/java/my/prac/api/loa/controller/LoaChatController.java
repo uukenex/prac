250e30528e8f7ddedcb02cf4ed727970d4b33b4b
thjeon <thjeon@172.16.217.72>
2024-10-17 17:08:55 +0900

    list separator

--- src/main/java/my/prac/api/loa/controller/LoaChatController.java
+++ src/main/java/my/prac/api/loa/controller/LoaChatController.java
@@ -407,9 +407,38 @@ public class LoaChatController {
 				List<String> list =botService.selectRoomList(reqMap);
 				
 				String tmp_val ="";
-				for(String s : list) {
-					tmp_val += s+listSeparatorStr;
+				
+				for(int i=0;i<list.size();i++) {
+					if(i!=0) {
+						tmp_val += listSeparatorStr;
+					}
+					tmp_val += list.get(i);
 				}
+				
+				if(tmp_val.equals("")) {
+					return "";
+				}
+				//tmp_val  = param1+tmp_val;
+				
+				/**  
+				  res = room1㉾room2
+				  
+				  if(res.indexOf(listSeparatorStr)){
+				  	var room_list = res.split("㉾")
+				    //room_list[0] =room1
+				    //room_list[1] =room2
+				    
+				    for (room:room_list){ 
+					  Api.replyRoom(room, 공지내용);
+					}
+				    
+				  }else{
+				   원래로직
+				  }
+				  =>
+				  
+				  
+				 **/
 				
 				val = tmp_val;
 				break;
