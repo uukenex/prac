4c690cc1f229a753c37d802d23394bcab7c7c2b1
thjeon <thjeon@172.16.217.72>
2025-03-13 15:42:58 +0900

    gpt 원복

--- src/main/java/my/prac/core/util/ChatGPTUtils.java
+++ src/main/java/my/prac/core/util/ChatGPTUtils.java
@@ -136,7 +136,7 @@ public class ChatGPTUtils {
 		
 
 		String json = "{\n" +
-		        "    \"model\": \"gpt-4.5-preview\",\n" +
+		        "    \"model\": \"gpt-4o\",\n" +
 		        "    \"messages\": [\n" +
 		        "        {\n" +
 		        "            \"role\": \"system\",\n" +
