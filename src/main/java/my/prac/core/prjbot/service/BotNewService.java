d8a396828a4b0d3abf739f7a3af2b64695374189
thjeon <thjeon@172.16.217.72>
2025-09-22 23:10:17 +0900

    NEWPOINT로직

---
+++ src/main/java/my/prac/core/prjbot/service/BotNewService.java
@@ -0,0 +1,11 @@
+package my.prac.core.prjbot.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface BotNewService {
+	
+} 
+
+	
---
+++ src/main/java/my/prac/core/prjbot/service/impl/BotNewServiceImpl.java
@@ -0,0 +1,20 @@
+package my.prac.core.prjbot.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import my.prac.core.prjbot.dao.BotDAO;
+import my.prac.core.prjbot.dao.BotSettleDAO;
+import my.prac.core.prjbot.service.BotNewService;
+
+@Service("core.prjbot.BotSettleService")
+public class BotNewServiceImpl implements BotNewService {
+	
+	@Resource(name = "core.prjbot.BotDAO")
+	BotDAO botDAO;
+	
+	@Resource(name = "core.prjbot.BotSettleDAO")
+	BotSettleDAO botSettleDAO;
+	
+}
