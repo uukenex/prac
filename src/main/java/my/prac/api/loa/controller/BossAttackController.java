d7f3a92a648621b8b6fd4f7fdfbd9faaaa80a68d
uukenex <uukenex@kakao.com>
2026-06-22 13:34:14 +0900

    문구조금 수정

--- src/main/java/my/prac/api/loa/controller/BossAttackController.java
+++ src/main/java/my/prac/api/loa/controller/BossAttackController.java
@@ -4789,7 +4789,7 @@ public class BossAttackController {
 			if (macroLock != null) {
 				String code = Objects.toString(macroLock.get("CODE"), "");
 				try { botNewService.insertMacroDetectLog(s.userName, code); } catch (Exception ignore) {}
-				return s.userName + "님, 비정상 패턴이 감지되었습니다.\n/매크로아님 " + code + " 를 입력하세요.";
+				return s.userName + "님, 비정상 패턴이 감지되었습니다."+NL+"/매크로아님 " + code + " 를 입력하세요.";
 			}
 		} catch (Exception ignore) {}
 		s.param1 = Objects.toString(s.map.get("param1"), "");
